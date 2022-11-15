9a086825b71e1966c2ce21977a0fb275eb3238c3
Black Pan <psxjoy@outlook.com>
2022-11-15 21:18:10 +0800

    Create 1.java

---
+++ HOT/Day1/1.java
@@ -0,0 +1,17 @@
+//1.两数之和
+//https://leetcode.cn/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = target - nums[i];
+            if (map.containsKey(tmp)) {
+                return new int[]{map.get(tmp), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
